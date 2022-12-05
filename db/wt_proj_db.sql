0a949b9c5f654dec205705beda77f5512034162f
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2022-12-05 12:18:51 +0530

    updated database backup

--- db/wt_proj_db.sql
+++ db/wt_proj_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2022 at 12:07 PM
+-- Generation Time: Dec 03, 2022 at 01:31 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -43,7 +43,10 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 (4, 'Java'),
 (5, 'JavaScript'),
 (6, 'HTML'),
-(7, 'JSON');
+(7, 'JSON'),
+(8, 'Ruby'),
+(9, 'Perl'),
+(10, 'ADBMS');
 
 -- --------------------------------------------------------
 
@@ -78,6 +81,7 @@ INSERT INTO `questions` (`id`, `question`, `option_1`, `option_2`, `option_3`, `
 --
 
 CREATE TABLE `results` (
+  `id` int(11) NOT NULL,
   `total_questions` int(11) NOT NULL,
   `attempt` int(11) NOT NULL,
   `not_attempt` int(11) NOT NULL,
@@ -91,11 +95,31 @@ CREATE TABLE `results` (
 -- Dumping data for table `results`
 --
 
-INSERT INTO `results` (`total_questions`, `attempt`, `not_attempt`, `right_answer`, `wrong_answer`, `user_id`, `categories_id`) VALUES
-(1, 1, 0, 1, 0, 1, 2),
-(2, 2, 0, 2, 0, 10, 1),
-(1, 1, 0, 0, 1, 10, 2),
-(2, 2, 0, 1, 1, 10, 1);
+INSERT INTO `results` (`id`, `total_questions`, `attempt`, `not_attempt`, `right_answer`, `wrong_answer`, `user_id`, `categories_id`) VALUES
+(1, 1, 1, 0, 1, 0, 1, 2),
+(2, 2, 2, 0, 2, 0, 10, 1),
+(3, 1, 1, 0, 0, 1, 10, 2),
+(4, 2, 2, 0, 1, 1, 10, 1),
+(5, 2, 2, 0, 1, 1, 15, 1),
+(6, 3, 2, 1, 2, 0, 15, 1),
+(7, 2, 2, 0, 0, 2, 15, 1),
+(8, 2, 2, 0, 1, 1, 17, 1),
+(9, 3, 3, 0, 2, 1, 17, 1),
+(10, 1, 1, 0, 1, 0, 18, 2),
+(11, 3, 3, 0, 1, 2, 18, 1),
+(12, 3, 2, 1, 2, 0, 3, 1),
+(13, 3, 3, 0, 1, 2, 3, 1),
+(14, 3, 1, 2, 0, 1, 18, 1),
+(15, 3, 3, 0, 1, 2, 19, 1),
+(16, 3, 0, 3, 0, 0, 19, 1),
+(17, 3, 0, 3, 0, 0, 19, 1),
+(18, 3, 0, 3, 0, 0, 19, 1),
+(19, 3, 0, 3, 0, 0, 19, 1),
+(20, 3, 0, 3, 0, 0, 19, 1),
+(21, 3, 0, 3, 0, 0, 19, 1),
+(22, 1, 1, 0, 0, 1, 2, 2),
+(23, 3, 3, 0, 2, 1, 20, 1),
+(24, 2, 2, 0, 2, 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -128,7 +152,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `image`, `is_superuser`)
 (10, 'user9', 'user9@gmail.com', 'asdf@123', 'basic.jpeg', NULL),
 (11, 'user10', 'user10@gmail.com', 'asdf@123', 'Nutritionoffer1.jpeg', NULL),
 (13, 'user11', 'user11@gmail.com', 'asdf@123', 'testing3.jpg', NULL),
-(14, 'admin', 'admin@gmail.com', 'asdf@123', 'yogaoffer2.jpg', 1);
+(14, 'admin', 'admin@gmail.com', 'asdf@123', 'yogaoffer2.jpg', 1),
+(15, 'pushpa', 'pushpa@gmail.com', 'asdf@123', 'DP girl.jpg', NULL),
+(17, 'user15', 'user15@gmail.com', 'asdf@123', 'Nutritionoffer3.jpg', NULL),
+(18, 'user6', 'user16@gmail.com', 'asdf@123', 'Nutritionoffer3.jpg', NULL),
+(19, 'user17', 'user17@gmail.com', 'asdf@123', 'DP.png', NULL),
+(20, 'sakshi', 'sakshi@gmail.com', 'asdf@123', 'DP girl.jpg', NULL);
 
 --
 -- Indexes for dumped tables
@@ -144,7 +173,16 @@ ALTER TABLE `categories`
 -- Indexes for table `questions`
 --
 ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categories_id` (`categories_id`);
+
+--
+-- Indexes for table `results`
+--
+ALTER TABLE `results`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `categories_id` (`categories_id`);
 
 --
 -- Indexes for table `users`
@@ -161,19 +199,42 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `results`
+--
+ALTER TABLE `results`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `Fk_categories_questions` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `results`
+--
+ALTER TABLE `results`
+  ADD CONSTRAINT `Fk_categories_results` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `Fk_user_results` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
